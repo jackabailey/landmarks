dea0bae39aa8886446516ab6ada786b13b9d15ab
Jack Bailey <services@bailey.ws>
2022-06-07 20:03:00 +0100

    Merge Master to Main

---
+++ Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jack Bailey on 06/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Views/landmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkView.swift
+//  Landmarks
+//
+//  Created by Jack Bailey on 07/06/2022.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+            LandmarkRow(landmark: landmarks[3])
+            LandmarkRow(landmark: landmarks[4])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
