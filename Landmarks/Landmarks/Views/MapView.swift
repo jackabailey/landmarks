05c1b5fea0c41a8454432632f996cc41acc9b412
Jack Bailey <services@bailey.ws>
2022-06-07 16:51:11 +0100

    Created Landmark Data Model

--- Landmarks/Landmarks/Views/MapView.swift
+++ Landmarks/Landmarks/Views/MapView.swift
@@ -11,7 +11,7 @@ import MapKit
 struct MapView: View {
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
         )
         
     var body: some View {
